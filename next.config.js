58e64c9d6bd230a6bf3fbda898c6ed9b2bbbb36c
markmazurk <markmazurk@gmail.com>
2024-03-15 13:30:36 -0700

    added server file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/photoai",
+  assetPrefix: "/photoai",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
